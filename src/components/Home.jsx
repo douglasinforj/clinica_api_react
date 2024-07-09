13375f65a25ef6a1efb4746c4fdc23bd0879b213
Douglas Silva <douglasitpro@gmail.com>
2024-07-09 17:41:55 -0300

    Troca de Logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="text-center">
             <Navbar />
             <br></br>
-            <img src='/images/logo.png' alt='Logo' className='img-fluid mb-4' style={{ maxWidth: '200px', marginTop: '50px' }} />
+            <img src='/images/logo.png' alt='Logo' className='img-fluid mb-4' style={{ maxWidth: '350px', marginTop: '50px' }} />
             <h1 class="mt-4">EXAMCLIN</h1>
             <p class="lead">Utilize o menu acima para navegar pelas opções de cadastro de clientes e marcação de exames.</p>
         </div>
